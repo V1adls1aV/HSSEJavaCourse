1a645bbfc26b9223b2e51d4a28f64ae53c4eb73b
v1adls1av <V1adls1aV@yandex.ru>
2025-02-09 17:34:28 +0300

    Spring Boot structure

---
+++ homework01/src/test/java/me/vladislav/homework01/TestHomework01Application.java
@@ -0,0 +1,11 @@
+package me.vladislav.homework01;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestHomework01Application {
+
+  public static void main(String[] args) {
+    SpringApplication.from(Homework01Application::main).with(TestcontainersConfiguration.class).run(args);
+  }
+
+}
