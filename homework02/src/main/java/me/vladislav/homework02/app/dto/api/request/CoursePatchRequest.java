4e2e382ff9cbfbbf7c5ed1062432e7c76698eb50
v1adls1av <V1adls1aV@yandex.ru>
2025-02-17 17:20:07 +0300

    Missed files

---
+++ homework02/src/main/java/me/vladislav/homework02/app/dto/api/request/CoursePatchRequest.java
@@ -0,0 +1,23 @@
+package me.vladislav.homework02.app.dto.api.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record CoursePatchRequest(
+    @NotNull
+    @Min(1)
+    Long id,
+
+    @Size(min = 3, max = 50)
+    String title,
+
+    @Size(min = 3, max = 50)
+    String author,
+
+    String description,
+
+    @Min(1) @Max(366)
+    Integer duration) {
+}
